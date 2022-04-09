ea9d17ebcd604ff8dad2c3667a0e05a3895a2369
seanrogan <srogan88@gmail.com>
2022-04-09 16:03:05 -0400

    set up using CLI args to set server access point

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String serverPath = "https://accounts.spotify.com";
         Scanner scan = new Scanner(System.in);
         Controller controller = new Controller(new Service());
-        if(args[0] == "-access"){
-
+        if(args[0].equals("-access")){
+        serverPath = args[1];
         }
         while (!Controller.isExit()) {
             controller.execute(scan.nextLine().split(" +"));
